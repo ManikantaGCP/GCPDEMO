41aa03c69182550af28ce95ce34b0ad114a5dcdc
ManikantaGCP <101719476+ManikantaGCP@users.noreply.github.com>
2022-03-16 18:35:54 +0530

    Update Partner.sh

--- Green/Partner.sh
+++ Green/Partner.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install apache2 wget -y
 cd /var/www/html
 rm -rf index.html
-wget <https://raw.githubusercontent.com/ManikantaGCP/GCPDEMO/main/Green/Index.html>
+wget https://raw.githubusercontent.com/ManikantaGCP/GCPDEMO/main/Green/Index.html
 sudo service apache2 restart
