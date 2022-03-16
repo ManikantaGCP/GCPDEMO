bf4e11d36d75234d1ebeb767e6a8e012cd95153e
ManikantaGCP <101719476+ManikantaGCP@users.noreply.github.com>
2022-03-16 18:36:45 +0530

    Update Red.sh

--- Whilte/Red/Red.sh
+++ Whilte/Red/Red.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install apache2 wget -y
 cd /var/www/html
 rm -rf index.html
-wget <URL>
+wget https://raw.githubusercontent.com/ManikantaGCP/GCPDEMO/main/Whilte/Red/Index.html
 sudo service apache2 restart
